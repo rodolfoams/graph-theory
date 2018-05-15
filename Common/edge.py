c09a829017855320bf1c922c2347d128a922efd8
Rodolfo Silva <rodolfo@grupomarinho.com.br>
2018-05-15 16:47:16 -0300

    Add directed edges

--- Common/edge.py
+++ Common/edge.py
@@ -1,11 +1,13 @@
 class Edge():
-    def __init__(self, weight, origin, destination):
+    def __init__(self, weight, origin, destination, directed=False):
         self.weight = weight
         self.origin = origin
         self.destination = destination
+        self.directed = directed
 
     def __gt__(self, other):
         return self.weight > other.weight
     
     def __str__(self):
-        return self.origin.name + "<->" + self.destination.name
+        arrow = "->" if self.directed else "<->"
+        return self.origin.name + arrow + self.destination.name + ", " + str(self.weight)
